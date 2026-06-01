a4dc94e0ad020ae19e255603d55ebce9ec395be5
hanyu <1619020076@qq.com>
2026-06-01 22:10:49 +0800

    20260531-v21

--- src/projects/video-rank/config.js
+++ src/projects/video-rank/config.js
@@ -6,5 +6,5 @@ export const VIDEO_RANK_PAGE = {
 }
 
 export const VIDEO_RANK_VERSION = 'v0.0.10'
-export const VIDEO_RANK_SPLASH_IMAGE_URL = 'https://assets.zice8.com/video-rank/vr_6f9k2q8m4/splash/splash.png'
+export const VIDEO_RANK_SPLASH_IMAGE_URL = 'https://assets.zice8.com/video-rank/vr_6f9k2q8m4x/splash/splash.png'
 export const VIDEO_RANK_SPLASH_AUTO_ENTER_MS = 3000
