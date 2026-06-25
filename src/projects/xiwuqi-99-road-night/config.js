2fd16ba1e3d02e3f10e4f72f04111b8f509be916
hanyu <1619020076@qq.com>
2026-06-25 10:00:56 +0800

    Use xiwuqi mobile config target url

--- src/projects/xiwuqi-99-road-night/config.js
+++ src/projects/xiwuqi-99-road-night/config.js
@@ -2,21 +2,16 @@ export const XIWUQI_99_ROAD_NIGHT_ACTIVITY_TYPE = 'xiwuqi_99_road_night'
 export const XIWUQI_99_ROAD_NIGHT_ACTIVITY_KEY = 'xiwuqi_99_road_night_20260624'
 export const XIWUQI_99_ROAD_NIGHT_ASSETS_BASE_URL =
   'https://assets.zice8.com/xiwuqi_99_road_night/xiwuqi_99_road_night_20260624'
-export const XIWUQI_99_ROAD_NIGHT_TARGET_URL = 'https://v.douyin.com/ooOBXZXQd8Q/'
 
 export const DEFAULT_CONFIG = {
   assetsBaseUrl: XIWUQI_99_ROAD_NIGHT_ASSETS_BASE_URL,
   imageFile: 'main.png',
   imageAlt: '西乌旗99号公路之夜',
-  targetUrl: XIWUQI_99_ROAD_NIGHT_TARGET_URL,
+  targetUrl: 'https://v.douyin.com/ooOBXZXQd8Q/',
 }
 
 export function mergeConfig(publicConfig) {
-  return {
-    ...DEFAULT_CONFIG,
-    ...(publicConfig?.mobileConfig || {}),
-    targetUrl: XIWUQI_99_ROAD_NIGHT_TARGET_URL,
-  }
+  return { ...DEFAULT_CONFIG, ...(publicConfig?.mobileConfig || {}) }
 }
 
 export function assetUrl(baseUrl, filename) {
