f2c16ab623ec2ee665fcf9166ded831b9b3d0ab0
hanyu <1619020076@qq.com>
2026-06-29 17:18:02 +0800

    Revert "fix: harden latex WeChat CTA rendering"
    This reverts commit a42c5d609bf1f70b1d988fe66ce1fe8f0b2256fc.

--- src/projects/latex-allergy-risk-test/index.jsx
+++ src/projects/latex-allergy-risk-test/index.jsx
@@ -32,7 +32,6 @@ const INTRO_CARDS = [
 ]
 
 const WECHAT_LAUNCH_OPTIONS = { openTagList: ['wx-open-launch-weapp'] }
-const SHOPPING_CART_ICON_DATA_URI = 'data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 1024 1024%22%3E%3Cpath fill=%22%23fff%22 d=%22M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 00-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 100 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 00-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 00-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6z%22/%3E%3C/svg%3E'
 
 function isWechatBrowser() {
   return typeof navigator !== 'undefined' && /MicroMessenger/i.test(navigator.userAgent)
@@ -359,15 +358,15 @@ function MiniProgramLaunchButton({ miniProgram, label, onFallback }) {
         transform-origin: center center;
         animation: latexCtaHeartbeat 2.4s ease-in-out infinite;
       }
-      .latex-mini-program-icon {
+      .latex-mini-program-button svg {
         flex: 0 0 auto;
         display: block;
         width: 18px;
         height: 18px;
         min-width: 18px;
-        background: url('${SHOPPING_CART_ICON_DATA_URI}') center / contain no-repeat;
+        overflow: visible;
       }
-      .latex-mini-program-button-label {
+      .latex-mini-program-button span {
         min-width: 0;
         overflow: hidden;
         text-overflow: ellipsis;
@@ -385,8 +384,8 @@ function MiniProgramLaunchButton({ miniProgram, label, onFallback }) {
       }
     </style>
     <button class="latex-mini-program-button" type="button">
-      <span class="latex-mini-program-icon" aria-hidden="true"></span>
-      <span class="latex-mini-program-button-label">${escapedLabel}</span>
+      <svg viewBox="0 0 1024 1024" focusable="false" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 00-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 100 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 00-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 00-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6z"></path></svg>
+      <span>${escapedLabel}</span>
     </button>
   `
 
