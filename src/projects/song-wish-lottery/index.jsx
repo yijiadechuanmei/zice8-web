0ec36a9a6374c1aea70ce474b718221b399c6c16
hanyu <1619020076@qq.com>
2026-07-20 02:10:38 +0800

    feat(song-wish-lottery): update draw action

--- src/projects/song-wish-lottery/index.jsx
+++ src/projects/song-wish-lottery/index.jsx
@@ -1,1 +1,10 @@
-export { default } from '../artist-call-lottery/index.jsx'
+import './styles.css'
+import ArtistCallLotteryProject from '../artist-call-lottery/index.jsx'
+
+export default function SongWishLotteryProject(props) {
+  return (
+    <div className="swl-copy-shell">
+      <ArtistCallLotteryProject {...props} />
+    </div>
+  )
+}
