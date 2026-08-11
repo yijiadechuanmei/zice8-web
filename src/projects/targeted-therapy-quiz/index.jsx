43c482fa425af9228cf37924d76ef6c6a5fe2c6d
hanyu <1619020076@qq.com>
2026-08-11 15:06:37 +0800

    feat: add playful targeted therapy title art

--- src/projects/targeted-therapy-quiz/index.jsx
+++ src/projects/targeted-therapy-quiz/index.jsx
@@ -19,6 +19,7 @@ import {
 import smileFace from './assets/smile-face.png'
 import sadFace from './assets/sad-face.png'
 import quizMascot from './assets/quiz-mascot.png'
+import homeLevelTitle from './assets/home-level-title.png'
 import './styles.css'
 
 const CATEGORY_ICONS = {
@@ -244,8 +245,7 @@ function HomePage({ categories, error, loading, onChoose }) {
         <i /><i /><i /><i /><i /><i /><i />
         <i /><i /><i /><i /><i /><i /><i />
       </div>
-      <h1 className="ttq-home-title">请选择您的关卡</h1>
-      <p className="ttq-home-tip">点击图标，开启随机知识挑战</p>
+      <img className="ttq-home-title" src={homeLevelTitle} alt="请选择您的关卡" />
       <div className="ttq-home-levels" aria-label="选择答题关卡">
         {categories.map((category, index) => (
           <button
