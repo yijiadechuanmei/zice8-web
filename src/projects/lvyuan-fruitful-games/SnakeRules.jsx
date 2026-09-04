8eea2de71ce8ecc857f876aee4e9fda4d4487766
hanyu <1619020076@qq.com>
2026-09-04 23:46:44 +0800

    fix: correct lvyuan snake rules actions

--- src/projects/lvyuan-fruitful-games/SnakeRules.jsx
+++ src/projects/lvyuan-fruitful-games/SnakeRules.jsx
@@ -12,11 +12,11 @@ export default function SnakeRules({ onBack, onStart }) {
         <PosterImage asset="background" className="lyfg-ih5-background" />
         <PosterImage asset="snakeRulesTitle" className="lyfg-ih5-snake-rules-title" alt="贪吃蛇游戏规则" />
         <PosterImage asset="snakeRulesDiagram" className="lyfg-ih5-snake-rules-diagram" alt="贪吃蛇游戏说明" />
-        <button className="lyfg-ih5-action lyfg-ih5-snake-rules-back" type="button" onClick={onBack} aria-label="返回游戏选择">
-          <PosterImage asset="snakeRulesBack" className="lyfg-ih5-fill-image" alt="返回游戏选择" />
+        <button className="lyfg-ih5-action lyfg-ih5-snake-rules-back" type="button" onClick={onStart} aria-label="开始贪吃蛇游戏">
+          <PosterImage asset="snakeRulesBack" className="lyfg-ih5-fill-image" alt="开始游戏" />
         </button>
-        <button className="lyfg-ih5-action lyfg-ih5-snake-rules-start" type="button" onClick={onStart} aria-label="开始贪吃蛇游戏">
-          <PosterImage asset="snakeRulesStart" className="lyfg-ih5-fill-image" alt="开始游戏" />
+        <button className="lyfg-ih5-action lyfg-ih5-snake-rules-start" type="button" onClick={onBack} aria-label="返回游戏选择">
+          <PosterImage asset="snakeRulesStart" className="lyfg-ih5-fill-image" alt="返回游戏选择" />
         </button>
       </Ih5Stage>
     </main>
